9711e0f62f5f91faa18d2ef11b8afdab009191ad
Blaire Coleman <68564883+blairecoleman@users.noreply.github.com>
2020-08-18 11:31:28 -0400

    Add files via upload

---
+++ php/config.php
@@ -0,0 +1,11 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'haitianhope');
+define('DB_PASSWORD', 'hopeforHaiti4444');
+define('DB_NAME', 'emails');
+
+
+session_start();
+require_once 'subscriptions.php';
+
+//error_reporting(0);
